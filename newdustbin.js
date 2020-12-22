691b2b01c2940802353ec828e284c3d33878bd4e
FredaFrancis <74528287+FredaFrancis@users.noreply.github.com>
2020-12-22 13:43:19 +0530

    Add files via upload

---
+++ newdustbin.js
@@ -0,0 +1,54 @@
+class dustbin
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=200;
+		this.dustbinHeight=100;
+		this.wallThickness=20;
+	
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+
+		
+
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+	
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			
+
+	push()
+	rectMode(CENTER)
+	fill(255)
+	stroke(255)
+     rect(posLeft.x, posLeft.y,this.wallThickness, this.dustbinHeight);
+	pop()
+
+	push()
+	rectMode(CENTER)
+	stroke(255)
+	 fill(255)
+	rect(posRight.x, posRight.y,this.wallThickness, this.dustbinHeight);
+	pop()
+
+	push()
+	rectMode(CENTER)
+    stroke(255)
+	fill(255)
+    rect(posBottom.x, posBottom.y,this.dustbinWidth, this.wallThickness);
+	pop()
+			
+	}
+
+}
